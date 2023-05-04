9277b88304bc478d2590c8e281db55d23f77a7ff
Jefferson545 <jefferson.avelar@catolica.edu.sv>
2023-05-03 22:01:09 -0600

    avanze1

---
+++ controladorclien.py
@@ -0,0 +1,34 @@
+from config import conectar
+
+def insertar_clientes(nombre_cliente,numero_telefono,direccion):
+    conexion= conectar()
+    with conexion.cursor() as cursor:
+        cursor.execute("INSERT INTO clientes(nombre_cliente,numero_telefono,direccion) VALUES (%s, %s, %s)",
+                       (nombre_cliente,numero_telefono,direccion))
+    conexion.commit()
+    conexion.close()
+
+def obtener_clientes():
+    conexion= conectar()
+    clientes = []
+    with conexion.cursor() as cursor:
+        cursor.execute("select id_clientes, nombre_cliente, numero_telefono, direccion from clientes")
+        clientes = cursor.fetchall()
+    conexion.close()
+    return clientes
+
+def eliminar_cliente(id_clientes):
+    conexion= conectar()
+    with conexion.cursor() as cursor:
+        cursor.execute("UPDATE clientes SET estado = 'I' where id_clientes=%s",
+                       (id_clientes))
+    conexion.commit()
+    conexion.close()
+
+def actualizar_clientes(nombre_cliente,numero_telefono,direccion,id_clientes):
+    conexion = conectar()
+    with conexion.cursor() as cursor:
+        cursor.execute("UPDATE clientes SET id_clientes = %s,nombre_cliente = %s,numero_telefono = %s, direccion = %s WHERE id_clientes = %s",
+                       (nombre_cliente,numero_telefono,direccion,id_clientes))
+    conexion.commit()
+    conexion.close()
