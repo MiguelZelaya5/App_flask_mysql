b6bd005ab13ba6936f455b8127f16b0473ba9f2f
Miguel <miguel.zelaya@catolica.edu.sv>
2023-05-06 20:10:46 -0600

    avance-hoysi2

--- controladorclien.py
+++ controladorclien.py
@@ -1,10 +1,10 @@
 from config import conectar
 
-def insertar_clientes(nombre_cliente,numero_telefono,direccion):
+def insertar_clientes(nombre_cliente,numero_telefono,direccion, estado):
     conexion= conectar()
     with conexion.cursor() as cursor:
-        cursor.execute("INSERT INTO clientes(nombre_cliente,numero_telefono,direccion) VALUES (%s, %s, %s)",
-                       (nombre_cliente,numero_telefono,direccion))
+        cursor.execute("INSERT INTO clientes(nombre_cliente,numero_telefono,direccion,estado) VALUES (%s, %s, %s,%s)",
+                       (nombre_cliente,numero_telefono,direccion,estado))
     conexion.commit()
     conexion.close()
 
@@ -25,17 +25,17 @@ def eliminar_cliente(id_clientes):
     conexion.commit()
     conexion.close()
 
-def actualizar_clientes(nombre_cliente,numero_telefono,direccion,id_clientes):
+def actualizar_clientes(nombre_cliente,numero_telefono,direccion,estado,id_clientes):
     conexion = conectar()
     with conexion.cursor() as cursor:
-        cursor.execute("UPDATE clientes SET nombre_cliente = %s,numero_telefono = %s, direccion = %s WHERE id_clientes = %s",
-                       (nombre_cliente,numero_telefono,direccion,id_clientes))
+        cursor.execute("UPDATE clientes SET nombre_cliente = %s,numero_telefono = %s, direccion = %s, estado=%s WHERE id_clientes = %s",
+                       (nombre_cliente,numero_telefono,direccion,estado,id_clientes))
     conexion.commit()
     conexion.close()
 
 def obtener_cliente_por_id(id_cliente):
     conexion = conectar()
     cursor=conexion.cursor()
-    cursor.execute("SELECT  id_clientes, nombre_cliente, numero_telefono, direccion from clientes WHERE id_clientes = %s",(id_cliente))
+    cursor.execute("SELECT  id_clientes, nombre_cliente, numero_telefono, direccion, estado from clientes WHERE id_clientes = %s",(id_cliente))
     actu= cursor.fetchone()
     return actu
